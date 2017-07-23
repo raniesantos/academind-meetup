dea1ccaed7717723e519ae24b30a19ead0cbf667
Ranie Santos <ranietots32@gmail.com>
2017-07-23 14:07:40 +0800

    added carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,50 @@
 <template>
-  <div>
-    <p>the home page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetups/create" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item v-for="meetup in meetups" :src="meetup.imageUrl" :key="meetup.id">
+            <div class="title">{{ meetup.title }}</div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome meetups</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        meetups: [
+          { imageUrl: 'http://www.itowns-project.org/images/sample_itowns.jpg', id: 'sadghjkl', title: 'meetup here' },
+          { imageUrl: 'http://spotify.github.io/HubFramework/resources/getting-started-tokyo.jpg', id: '37689', title: 'meetup there' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: #fff;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
