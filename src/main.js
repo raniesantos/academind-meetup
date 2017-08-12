ef48a37e7273d8a8a754ddcf56baab8dc0c13936
Ranie Santos <ranietots32@gmail.com>
2017-08-13 03:57:24 +0800

    installed firebase, created signup form

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
+import * as firebase from 'firebase'
 import router from './router'
 import store from './store'
 import DateFilter from './filters/date'
@@ -15,5 +16,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB6_a17VO_rX8sdui8wGmRkGi3l6zECaYc',
+      authDomain: 'vuemeetup-127e6.firebaseapp.com',
+      databaseURL: 'https://vuemeetup-127e6.firebaseio.com',
+      projectId: 'vuemeetup-127e6',
+      storageBucket: 'vuemeetup-127e6.appspot.com'
+    })
+  }
 })
